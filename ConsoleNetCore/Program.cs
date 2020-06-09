7fee8b9974abba1fdd254d51bac01636a6e82bb0
Two <Two.Logician@icloud.com>
2020-06-09 22:23:09 +0700

    :sparkles:

--- ConsoleNetCore/Program.cs
+++ ConsoleNetCore/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using SignLib.Certificates;
 using SignLib.Pdf;
 using Softveloper.IO;
@@ -10,6 +13,33 @@ namespace ConsoleNetCore
     class Program
     {
         static void Main(string[] args)
+        {
+            if (args.Any(x => x == "--download"))
+            {
+                Download();
+            }
+            if (args.Any(x => x == "--sign-pdf"))
+            {
+                SignPdf();
+            }
+            if (args.Any(x => x == "--upload"))
+            {
+                Upload();
+            }
+        }
+
+        public static void Download()
+        {
+            using (var client = new HttpClient())
+            using (var fs = new FileStream($"./temps/{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.pdf", FileMode.OpenOrCreate))
+            {
+                var rs = client.GetAsync("https://asp.demosoft.me/api/files?fileName=source.pdf").Result;
+                var stream = rs.Content.ReadAsStreamAsync().Result;
+                stream.CopyTo(fs);
+            }
+        }
+
+        public static void SignPdf()
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
@@ -31,6 +61,20 @@ namespace ConsoleNetCore
             ps.SigningReason = "I approve this document";
             Filerectory.CreateDirectory("./temps");
             File.WriteAllBytes($"./temps/{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.pdf", ps.ApplyDigitalSignature());
+        }
+
+        public static void Upload()
+        {
+            using (var client = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent())
+                using (var stream = new FileStream("./source.pdf", FileMode.Open))
+                {
+                    content.Add(new StreamContent(stream), "file", "source.pdf");
+                    var message = client.PostAsync("https://asp.demosoft.me/api/files", content).Result;
+                    Console.WriteLine(message.Content);
+                }
+            }
         }
     }
 }
